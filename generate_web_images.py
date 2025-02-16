7cf608aa40480429201ecc9ed47a712c9eaee493
Gimle Larpes <gimlelarpes@gmail.com>
2025-02-16 01:50:47 +0100

    added unlimited resolutions

--- generate_web_images.py
+++ generate_web_images.py
@@ -1,71 +1,60 @@
 #!/usr/bin/env python
 
-#Create .webp and .jpg images in sizes: 3840, 2560, 1920, 1600, 1440, 1280, 1080, 720, 480, 360
+# Create .webp and .jpg images in variable sizes
 from gimpfu import *
 import os
 
-def generate_web_images(image, drawable, filename, res_3840, res_2560, res_1920, res_1600, res_1440, res_1280, res_1080, res_720, res_480, res_360, jpeg_quality, jpeg_smooth, webp_quality, savedir, COPYRIGHT):
+def generate_web_images(image, drawable, filename, resolutions, jpeg_quality, jpeg_smooth, webp_quality, savedir, comment):
     pdb.gimp_image_undo_group_start(image)
-    #Aspect Ratio
+
+    # Aspect ratio
     h = float(pdb.gimp_image_height(image))
     w = float(pdb.gimp_image_width(image))
     ASPECT_RATIO = float(w / h)
 
-    #JPEG SETTINGS
+    # JPEG settings
     jpeg_quality = float(jpeg_quality/100.0)
     jpeg_smooth =  float(jpeg_smooth/100.0)
-    #WEBP SETTINGS
+    # WEBP settings
     webp_quality = float(webp_quality)
 
 
-    ####Create Images
-    #Create 3840 images if possible
-    generate_images(image, drawable, savedir, filename, w, ASPECT_RATIO, 3840, res_3840, jpeg_quality, jpeg_smooth, webp_quality, COPYRIGHT)
-    #Create 2560 images if possible
-    generate_images(image, drawable, savedir, filename, w, ASPECT_RATIO, 2560, res_2560, jpeg_quality, jpeg_smooth, webp_quality, COPYRIGHT)
-    #Create 1920 images if possible
-    generate_images(image, drawable, savedir, filename, w, ASPECT_RATIO, 1920, res_1920, jpeg_quality, jpeg_smooth, webp_quality, COPYRIGHT)
-    #Create 1600 images if possible
-    generate_images(image, drawable, savedir, filename, w, ASPECT_RATIO, 1600, res_1600, jpeg_quality, jpeg_smooth, webp_quality, COPYRIGHT)
-    #Create 1440 images if possible
-    generate_images(image, drawable, savedir, filename, w, ASPECT_RATIO, 1440, res_1440, jpeg_quality, jpeg_smooth, webp_quality, COPYRIGHT)
-    #Create 1280 images if possible
-    generate_images(image, drawable, savedir, filename, w, ASPECT_RATIO, 1280, res_1280, jpeg_quality, jpeg_smooth, webp_quality, COPYRIGHT)
-    #Create 1080 images if possible
-    generate_images(image, drawable, savedir, filename, w, ASPECT_RATIO, 1080, res_1080, jpeg_quality, jpeg_smooth, webp_quality, COPYRIGHT)
-    #Create 720 images if possible
-    generate_images(image, drawable, savedir, filename, w, ASPECT_RATIO, 720, res_720, jpeg_quality, jpeg_smooth, webp_quality, COPYRIGHT)
-    #Create 480 images if possible
-    generate_images(image, drawable, savedir, filename, w, ASPECT_RATIO, 480, res_480, jpeg_quality, jpeg_smooth, webp_quality, COPYRIGHT)
-    #Create 360 images if possible
-    generate_images(image, drawable, savedir, filename, w, ASPECT_RATIO, 360, res_360, jpeg_quality, jpeg_smooth, webp_quality, COPYRIGHT)
-
+    # Create images
+    resolutions = resolutions.replace(","," ")
+    resolutions = "".join(c for c in resolutions if (c.isdigit() or c ==" "))
+    resolutions = sorted(list(map(int, resolutions.split())), reverse=True)
+    max_width = max(resolutions)
+    for resolution in resolutions:
+        generate_images(image, drawable, savedir, filename, w, ASPECT_RATIO, resolution, max_width, jpeg_quality, jpeg_smooth, webp_quality, comment)
+    
     pdb.gimp_image_undo_group_end(image)
 
 
+def generate_images(image, drawable, savedir, filename, picture_width, ASPECT_RATIO, scale_width, max_width, jpeg_quality, jpeg_smooth, webp_quality, comment):
+    # Filler digits
+    max_slength = len(str(max_width))
+    scale_slength = len(str(scale_width))
+    d_slength = max_slength - scale_slength
+    if d_slength > 0:
+        filler_digit = "0" * d_slength
+    else:
+        filler_digit = ""
 
-####FUNCTIONS####
-def generate_images(image, drawable, savedir, filename, picture_width, ASPECT_RATIO, scale_width, enabled, jpeg_quality, jpeg_smooth, webp_quality, COPYRIGHT):
-    #Create image if possible
-    if picture_width >= scale_width and enabled:
-        if scale_width >= 1000:
-            filler_digit = ""
-        else:
-            filler_digit = "0"
+    # Create image if possible
+    if picture_width >= scale_width:
         pdb.gimp_image_scale(image, scale_width, int(round(scale_width/ASPECT_RATIO)))
-        #Save JPEG
-        pdb.file_jpeg_save(image, drawable, os.path.join(savedir,(filename+"-"+str(filler_digit)+str(scale_width)+".jpeg")), filename+"-"+str(filler_digit)+str(scale_width)+".jpeg", jpeg_quality, jpeg_smooth, 1, 1, COPYRIGHT, 1, 0, 128, 2)
-        #Save WEBP
+
+        # Save JPEG
+        pdb.file_jpeg_save(image, drawable, os.path.join(savedir,(filename+"-"+str(filler_digit)+str(scale_width)+".jpeg")), filename+"-"+str(filler_digit)+str(scale_width)+".jpeg", jpeg_quality, jpeg_smooth, 1, 1, comment, 1, 0, 128, 2)
+        
+        # Save WEBP
         pdb.file_webp_save2(image, drawable, os.path.join(savedir,(filename+"-"+str(filler_digit)+str(scale_width)+".webp")), filename+"-"+str(filler_digit)+str(scale_width)+".webp", 2, 0, webp_quality, 0.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-#################
-
-
 
 
 register(
     "python-fu-generate-web-images",
     "Generate Images For Websites",
-    "Creates .webp and .jpg images with horizontal resolution: 3840, 2560, 1920, 1600, 1440, 1080, 720, 480, 360. Images are exported to given folder or reverts to 'Pictures' folder.",
+    "Creates .webp and .jpg images for use on the web. Images are exported to the provided folder or to 'Pictures' if no folder is provided.",
     "Gimle Larpes",
     "Gimle Larpes",
     "2023",
@@ -75,25 +64,17 @@ register(
     (PF_IMAGE, "image", "takes current image", None),
     (PF_DRAWABLE, "drawable", "Input layer", None),
     (PF_STRING, "filename", "Image Name:", "Image"),
-    (PF_BOOL, "res_3840", "3840:", False),
-    (PF_BOOL, "res_2560", "2560:", True),
-    (PF_BOOL, "res_1920", "1920:", True),
-    (PF_BOOL, "res_1600", "1600:", False),
-    (PF_BOOL, "res_1440", "1440:", False),
-    (PF_BOOL, "res_1280", "1280:", True),
-    (PF_BOOL, "res_1080", "1080:", False),
-    (PF_BOOL, "res_720", "720:", False),
-    (PF_BOOL, "res_480", "480:", False),
-    (PF_BOOL, "res_360", "360:", False),
+    (PF_STRING, "resolutions", "Horizontal Resolutions:", "720, 1280, 1920"),
     (PF_SLIDER, "jpeg_quality", "JPEG Quality:", 85, (0, 100, 1)),
     (PF_SLIDER, "jpeg_smooth", "JPEG Smoothing:", 25, (0, 100, 1)),
     (PF_SLIDER, "webp_quality", "WebP Quality:", 80, (0, 100, 1)),
     (PF_DIRNAME, "savedir", "Export Directory:", os.path.join(os.environ['USERPROFILE'],"Pictures")),
-    (PF_STRING, "COPYRIGHT", "Copyright Notice:", ""),    #COPYRIGHT NOTICE (jpeg)
+    (PF_STRING, "comment", "Image Comment:", ""),  # (jpeg only)
     ],
     [],
     generate_web_images,
     menu="<Image>/Image"
     )
+
 
 main()
